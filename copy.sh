108cc93ee8593983ca8a54fa148b75c3bc6d1b73
mrdotx <klassiker@gmx.de>
2020-02-23 20:55:11 +0100

    small fixes

--- copy.sh
+++ copy.sh
@@ -3,36 +3,25 @@
 # path:       ~/projects/iptv/copy.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/iptv
-# date:       2020-02-03T13:18:21+0100
-
-# color variables
-#black=$(tput setaf 0)
-#red=$(tput setaf 1)
-#green=$(tput setaf 2)
-#yellow=$(tput setaf 3)
-#blue=$(tput setaf 4)
-magenta=$(tput setaf 5)
-#cyan=$(tput setaf 6)
-#white=$(tput setaf 7)
-reset=$(tput sgr0)
+# date:       2020-02-23T20:54:32+0100
 
 iptv=$HOME/projects/iptv
 
 # copy to webserver
-echo "[${magenta}info${reset}] Copy to hermes..."
+printf ":: copy to hermes...\n"
 rsync --info=progress2 -ac \
     --exclude="copy.sh" \
     --exclude="LICENSE.md" \
     --exclude="README.md" \
     "$iptv/" alarm@hermes:/srv/http/iptv/
 
-echo "[${magenta}info${reset}] Copy to prometheus..."
+printf ":: copy to prometheus...\n"
 rsync --info=progress2 -ac \
     --exclude="copy.sh" \
     --exclude="LICENSE.md" \
     --exclude="README.md" \
     "$iptv/" alarm@prometheus:/srv/http/iptv/
 
-echo "[${magenta}info${reset}] Copy completed!"
+printf ":: copy completed!\n"
 
 notify-send "IpTV" "Copy complete!" --icon=messagebox_info
