a8d52b7c65c4bbba8cb3d4b796e1f252efad4999
mrdotx <klassiker@gmx.de>
2020-10-12 09:02:52 +0200

    from copy to move to webserver added

--- epg.sh
+++ epg.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/epg/epg.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/epg
-# date:       2020-06-02T10:54:07+0200
+# date:       2020-10-11T11:02:51+0200
 
 source_file="/home/alarm/wg++/guide.xml"
 destination_file="/srv/http/epg/epg.xml"
@@ -11,10 +11,9 @@ destination_file="/srv/http/epg/epg.xml"
 # execute webgrab++
 wg++
 
-# copy to webserver
-rm $destination_file
-cp $source_file $destination_file
+# move to webserver
+mv -f $source_file $destination_file
 chmod 755 $destination_file
 
-# copy to hermes
-rsync -acqP $destination_file alarm@hermes:/srv/http/epg/epg.xml
+# copy to 2nd webserver
+rsync -acqP $destination_file alarm@hermes:$destination_file
