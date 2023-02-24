8bdf985a55a48c438b225c7659f359bf63adda70
mrdotx <klassiker@gmx.de>
2023-02-24 10:17:56 +0100

    copy m3u to webserver added

--- epg.sh
+++ epg.sh
@@ -3,30 +3,42 @@
 # path:   /home/klassiker/.local/share/repos/epg/epg.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/epg
-# date:   2023-01-20T12:57:24+0100
+# date:   2023-02-24T10:16:32+0100
 
-source_file="$HOME/wg++/epg.xml"
-destination_file="/srv/http/download/epg/epg.xml"
-
-# execute webgrab++
-wg++
+source_epg="$HOME/wg++/epg.xml"
+destination_epg="/srv/http/download/epg/epg.xml"
+source_m3u="$HOME/.local/share/repos/epg/xitylight.m3u"
+destination_m3u="/srv/http/download/epg/channels.m3u"
 
 # helper
 copy_local() {
     rsync -acqPh \
         --chmod=F755 \
         --remove-source-files \
-        "$source_file" "$destination_file" \
-        && printf "%s copied to %s\n" "$source_file" "$destination_file"
+        "$source_epg" "$destination_epg" \
+        && printf "%s copied to %s\n" "$source_epg" "$destination_epg"
+
+    rsync -acqPh \
+        --chmod=F755 \
+        "$source_m3u" "$destination_m3u" \
+        && printf "%s synced to %s\n" "$source_m3u" "$destination_m3u"
 }
 
 copy_remote() {
     rsync -acqPh \
         --chmod=F755 \
         --remove-source-files \
-        "$source_file" "$1":$destination_file \
-        && printf "%s copied to %s:%s\n" "$source_file" "$1" "$destination_file"
+        "$source_epg" "$1":$destination_epg \
+        && printf "%s copied to %s:%s\n" "$source_epg" "$1" "$destination_epg"
+
+    rsync -acqPh \
+        --chmod=F755 \
+        "$source_m3u" "$1":$destination_m3u \
+        && printf "%s synced to %s\n" "$source_m3u" "$destination_m3u"
 }
+
+# execute webgrab++
+wg++
 
 # copy to webserver
 copy_local
