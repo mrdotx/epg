af05bf80ddd5f5db1893fcc5f88e8e8c48bc921d
mrdotx <klassiker@gmx.de>
2024-01-05 09:10:48 +0100

    description corrected

--- epg.sh
+++ epg.sh
@@ -3,15 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/epg/epg.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/epg
-# date:   2023-06-07T07:47:28+0200
+# date:   2024-01-04T12:12:59+0100
 
 # helper
-copy_file() {
+move_file() {
     rsync -acqPh \
         --chmod=F644 \
         --remove-source-files \
         "$1" "$2" \
-        && printf "%s copied to %s\n" "$1" "$2"
+        && printf "%s moved to %s\n" "$1" "$2"
 }
 
 sync_file() {
@@ -25,7 +25,7 @@ sync_file() {
 wg++
 
 # transfer to webserver
-copy_file \
+move_file \
     "$HOME/wg++/epg.xml" \
     "/srv/http/download/epg/epg.xml"
 sync_file \
