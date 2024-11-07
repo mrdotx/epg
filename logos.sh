8e0292b4fe995d8509be056fe73006a35d7b8337
mrdotx <klassiker@gmx.de>
2024-11-07 06:32:46 +0100

    initial upload

---
+++ logos.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# path:   /home/klassiker/.local/share/repos/epg/logos.sh
+# author: klassiker [mrdotx]
+# github: https://github.com/mrdotx/shell
+# date:   2024-11-06T05:19:26+0100
+
+# speed up script and avoid language problems by using standard c
+LC_ALL=C
+LANG=C
+
+# config
+channel_list="$HOME/.local/share/repos/epg/playlists/xitylight.m3u"
+
+awk '
+        /tvg-logo=/{
+            match($0,/tvg-logo="[^"]*/);
+            num=split(substr($0,RSTART,RLENGTH),a,"=\"");
+            match($0,/tvg-id="[^"]*/);
+            num1=split(substr($0,RSTART,RLENGTH),b,"=\"");
+            print b[num],";",a[num1]
+        }' "$channel_list" \
+    | sed "s/ ; /;/g" \
+    | sort -u
