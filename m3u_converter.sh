dcaea87a7cefb843cbf2dbd8180a9d4b947bd802
mrdotx <klassiker@gmx.de>
2025-06-29 05:25:26 +0200

    logos.sh in m3u_converter.sh integrated

--- m3u_converter.sh
+++ m3u_converter.sh
@@ -3,9 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/epg/m3u_converter.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/epg
-# date:   2024-10-23T08:06:48+0200
+# date:   2025-06-29T05:23:39+0200
 
-ext="${1##*.}"
+# speed up script and avoid language problems by using standard c
+LC_ALL=C
+LANG=C
+
+# config
+file_ext=${1##*.}
+file_name=${1%."$file_ext"}
 
 # helper
 get_value() {
@@ -17,42 +23,69 @@ get_value() {
         | cut -d'"' -f2
 }
 
+m3u_to_csv_logos() {
+    # extract tvg-logo and tvg-id from channel list
+    awk '
+            /tvg-logo=/{
+                match($0,/tvg-logo="[^"]*/);
+                num=split(substr($0,RSTART,RLENGTH),a,"=\"");
+                match($0,/tvg-id="[^"]*/);
+                num1=split(substr($0,RSTART,RLENGTH),b,"=\"");
+                print b[num],";",a[num1]
+            }' "$1" \
+        | sed "s/ ; /;/g" \
+        | sort -u > "$2"
+}
+
+m3u_to_csv() {
+    printf "tvg-name\tgroup-title\ttvg-logo\ttvg-id\turi\n" > "$2"
+
+    grep -v "#EXTM3U\|#EXTVLCOPT\|^[[:space:]]*$" "$1" \
+        | while IFS= read -r line; do
+            name=$(get_value 'tvg-name' "$line")
+            group=$(get_value 'group-title' "$line")
+            logo=$(get_value 'tvg-logo' "$line")
+            id=$(get_value 'tvg-id' "$line")
+            IFS= read -r uri
+            printf "%s\t%s\t%s\t%s\t%s\n" \
+                "$name" \
+                "$group" \
+                "$logo" \
+                "$id" \
+                "$uri" >> "$2"
+        done
+}
+
+csv_to_m3u() {
+    printf "#EXTM3U\n" > "$2"
+
+    grep -v "tvg-name	group-title	tvg-logo	tvg-id	uri" "$1" \
+        | awk -F "\t" '{print "#EXTINF:-1 tvg-name=\"" $1 \
+                    "\" group-title=\"" $2 \
+                    "\" tvg-logo=\"" $3 \
+                    "\" tvg-id=\"" $4 \
+                    "\"," $1 "\n" $5}' >> "$2"
+}
+
 # main
-case $ext in
+case $file_ext in
     *m3u|*m3u8)
-        csv="${1%."$ext"}.csv"
+        output_file="$file_name.csv"
+        printf "convert: %s -> %s\n" "${1##*/}" "${output_file##*/}"
 
-        printf "tvg-name\tgroup-title\ttvg-logo\ttvg-id\turi\n" > "$csv"
-
-        grep -v "#EXTM3U\|#EXTVLCOPT\|^[[:space:]]*$" "$1" \
-            | while IFS= read -r line; do
-                name=$(get_value 'tvg-name' "$line")
-                group=$(get_value 'group-title' "$line")
-                logo=$(get_value 'tvg-logo' "$line")
-                id=$(get_value 'tvg-id' "$line")
-                IFS= read -r uri
-                printf "%s\t%s\t%s\t%s\t%s\n" \
-                    "$name" \
-                    "$group" \
-                    "$logo" \
-                    "$id" \
-                    "$uri" >> "$csv"
-            done
-
-        printf "\"%s\" converted to \"%s\"\n" "$1" "$csv"
+        case "$2" in
+            --logos)
+                m3u_to_csv_logos "$1" "$output_file"
+                ;;
+            *)
+                m3u_to_csv "$1" "$output_file"
+                ;;
+        esac
         ;;
     *csv)
-        m3u="${1%."$ext"}.m3u"
+        output_file="$file_name.m3u"
+        printf "convert: %s -> %s\n" "${1##*/}" "${output_file##*/}"
 
-        printf "#EXTM3U\n" > "$m3u"
-
-        grep -v "tvg-name	group-title	tvg-logo	tvg-id	uri" "$1" \
-            | awk -F "\t" '{print "#EXTINF:-1 tvg-name=\"" $1 \
-                        "\" group-title=\"" $2 \
-                        "\" tvg-logo=\"" $3 \
-                        "\" tvg-id=\"" $4 \
-                        "\"," $1 "\n" $5}' >> "$m3u"
-
-        printf "\"%s\" converted to \"%s\"\n" "$1" "$m3u"
+        csv_to_m3u "$1" "$output_file"
         ;;
 esac
